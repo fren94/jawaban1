476262f69c1572a9c1e255c0253d9fc813065661
fren94 <50772671+fren94@users.noreply.github.com>
2019-05-18 20:44:58 +0700

    Add files via upload

---
+++ jawabannomor1.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+using namespace std;
+char nama[15];
+char hoby[15];
+char is_married[15];
+char school[15];
+char name[15];
+char score[15];
+
+int main(){
+cout<<"Masukan Nama kamu :";
+cin>>nama;
+cout<<"Masukan hoby :";
+cin>>hoby;
+cout<<"is_married :";
+cin>>is_married;
+cout<<"school :";
+cin>>school;
+cout<<"name :";
+cin>>name;
+cout<<"score : ";
+cin>>score;
+
+cout<<"++++++++++++++++++++++++++++++++++++++";
+cout<<"biodata  kamu  "<<nama <<endl;
+cout<<"Nama   :"<<nama <<endl;
+cout<<"hoby   :"<<hoby <<endl;
+cout<<"is_married	:"<<is_married <<endl;
+cout<<"school   :"<<school<<endl;
+cout<<"name   :"<<name<<endl;
+cout<<"score   :"<<score<<endl;
+return 0;
+}
+
